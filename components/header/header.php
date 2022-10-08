49b29d81345ca3de5784907c9560bdb0fb4a97d8
DjSeanza <ghostikk2@gmail.com>
2022-10-08 20:36:48 +0200

    presmerovanie na login

--- components/header/header.php
+++ components/header/header.php
@@ -1,7 +1,7 @@
 <header class="header flex-row p-2">
     <div class="header-left">
         <button class="button">
-            <img src="img/hamburger-menu.svg" alt="Hamburger menu">
+            <img src="<?php echo ROOT_PATH; ?>img/hamburger-menu.svg" alt="Hamburger menu">
         </button>
         <div class="logo">
             <img src="img/logo.svg" alt="Logo">
@@ -12,13 +12,13 @@
             <div>
                 <input type="text" placeholder="Search" class="p-2">
                 <button class="button pl-4 pr-4">
-                    <img src="img/search-icon.svg" alt="">
+                    <img src="<?php echo ROOT_PATH; ?>img/search-icon.svg" alt="">
                 </button>
             </div>
         </form>
     </div>
     <div class="header-right">
-        <button class="button">
+        <button class="button" onclick='location.href="<?php echo ROOT_PATH; ?>pages/login/login.php"'>
 
         </button>
     </div>
