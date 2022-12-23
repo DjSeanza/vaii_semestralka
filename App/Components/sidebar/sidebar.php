bfdb0ca57cd67e8f2e413f4858ae61cfa7cafb63
DjSeanza <ghostikk2@gmail.com>
2022-12-23 22:23:56 +0100

    skrytie prihlasenia ak je uz prihlaseny

--- App/Components/sidebar/sidebar.php
+++ App/Components/sidebar/sidebar.php
@@ -1,9 +1,15 @@
+<?php
+/** @var \App\Core\IAuthenticator $auth */
+?>
+
 <aside class="sidebar">
     <div class="sidebar-container">
         <?php require dirname(__DIR__)."../nav/nav.php" ?>
     </div>
+    <?php if (!$auth->isLogged()) { ?>
     <div class="sidebar-container sidebar-login-container">
         <span>Ak chcete k videám pridať označenie páči sa mi, komentovať alebo sa prihlásiť na odber, musíte sa prihlásiť.</span>
         <button type="button" class="button">Prihlásiť sa</button>
     </div>
+    <?php } ?>
 </aside>
